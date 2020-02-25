140f62ddad2714f750eedb778d131a84ee1735e8
Grigore Stefan <g_stefan@yahoo.com>
2020-02-25 03:47:30 +0200

    initial

---
+++ source/quantum-script-extension-base64.cpp
@@ -0,0 +1,97 @@
+﻿//
+// Quantum Script Extension Base64
+//
+// Copyright (c) 2020 Grigore Stefan <g_stefan@yahoo.com>
+// Created by Grigore Stefan <g_stefan@yahoo.com>
+//
+// MIT License (MIT) <http://opensource.org/licenses/MIT>
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "quantum-script-extension-base64-license.hpp"
+#include "quantum-script-extension-base64.hpp"
+#ifndef QUANTUM_SCRIPT_EXTENSION_BASE64_NO_VERSION
+#include "quantum-script-extension-base64-version.hpp"
+#endif
+
+#include "quantum-script-variablestring.hpp"
+//
+#include "quantum-script-extension-buffer-variablebuffer.hpp"
+
+//#define QUANTUM_SCRIPT_VM_DEBUG_RUNTIME
+
+namespace Quantum {
+	namespace Script {
+		namespace Extension {
+			namespace Base64 {
+
+				using namespace XYO;
+				using namespace Quantum::Script;
+
+				static TPointer<Variable> encode(VariableFunction *function, Variable *this_, VariableArray *arguments) {
+#ifdef QUANTUM_SCRIPT_DEBUG_RUNTIME
+					printf("- base64-encode\n");
+#endif
+					return VariableString::newVariable(XYO::Base64::encode((arguments->index(0))->toString()));
+				};
+
+				static TPointer<Variable> decode(VariableFunction *function, Variable *this_, VariableArray *arguments) {
+#ifdef QUANTUM_SCRIPT_DEBUG_RUNTIME
+					printf("- base64-decode\n");
+#endif
+					String result;
+					if(XYO::Base64::decode((arguments->index(0))->toString(), result)) {
+						return VariableString::newVariable(result);
+					};
+					return Context::getValueUndefined();
+				};
+
+				static TPointer<Variable> decodeToBuffer(VariableFunction *function, Variable *this_, VariableArray *arguments) {
+#ifdef QUANTUM_SCRIPT_DEBUG_RUNTIME
+					printf("- base64-decode-to-buffer\n");
+#endif
+					String result;
+					if(XYO::Base64::decode((arguments->index(0))->toString(), result)) {
+						return Extension::Buffer::VariableBuffer::newVariableFromString(result);
+					};
+					return Context::getValueUndefined();
+				};
+
+				void registerInternalExtension(Executive *executive) {
+					executive->registerInternalExtension("Base64", initExecutive);
+				};
+
+				void initExecutive(Executive *executive, void *extensionId) {
+
+					String info = "Base64\r\n";
+					info << License::shortContent();
+
+					executive->setExtensionName(extensionId, "Base64");
+					executive->setExtensionInfo(extensionId, info);
+#ifndef QUANTUM_SCRIPT_EXTENSION_BASE64_NO_VERSION
+					executive->setExtensionVersion(extensionId, Extension::Base64::Version::versionWithBuild());
+#endif
+					executive->setExtensionPublic(extensionId, true);
+
+					executive->compileStringX("Script.requireExtension(\"Buffer\");");
+					executive->compileStringX("var Base64={};");
+
+					executive->setFunction2("Base64.encode(str)", encode);
+					executive->setFunction2("Base64.decode(str)", decode);
+					executive->setFunction2("Base64.decodeToBuffer(str)", decodeToBuffer);
+				};
+
+			};
+		};
+	};
+};
+
+#ifdef XYO_COMPILE_DYNAMIC_LIBRARY
+extern "C" QUANTUM_SCRIPT_EXTENSION_BASE64_EXPORT void quantumScriptExtension(Quantum::Script::Executive *executive, void *extensionId) {
+	Quantum::Script::Extension::Base64::initExecutive(executive, extensionId);
+};
+#endif
+
---
+++ test/test.0001.js
@@ -0,0 +1,34 @@
+// Public domain
+// http://unlicense.org/
+
+Script.requireExtension("Console");
+Script.requireExtension("Base64");
+
+var checkEncoding = [
+		["", ""],
+		["f", "Zg=="],
+		["fo", "Zm8="],
+		["foo", "Zm9v"],
+		["foob", "Zm9vYg=="],
+		["fooba", "Zm9vYmE="],
+		["foobar", "Zm9vYmFy"]
+	];
+
+var allOk = true;
+for(var k of checkEncoding) {
+	if(Base64.encode(k[0]) != k[1]) {
+		allOk = false;
+		Console.writeLn("Error-Encode: " + k[0] + " : " + k[1] + " : " + Base64.encode(k[0]));
+	};
+};
+
+for(var k of checkEncoding) {
+	if(Base64.decode(k[1]) != k[0]) {
+		allOk = false;
+		Console.writeLn("Error-Decode: " + k[1] + " : " + k[0] + " : " + Base64.decode(k[1]));
+	};
+};
+
+if(allOk) {
+	Console.writeLn("-> test 0001 ok");
+};
